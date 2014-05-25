9fd5794b410d088191a196a003f7a80b7ec7dd35
Eugenio Delfa <ed@isbox.org>
2014-05-25 00:24:01 -0500

    .

--- Flow.py
+++ Flow.py
@@ -1,8 +1,12 @@
-import re
 import sys
+import os
+import re
 import pydot
+import urllib
 from optparse import OptionParser
 
+# Class method/s execution flow (no outside class calls analized)
+##
 class GraphManager(object):
 	""" pydot graph objects manager """
 	def __init__(self):
@@ -21,30 +25,41 @@ class GraphManager(object):
 			ecolor = 'blue'
 		else:
 			ecolor = 'gray'
+
 		# node shape based on block type (First or Last instruction)
 		nodes = [None, None]
 		blocks = [b1,b2]
 		for i in range(2):
-			if Block.isTag(blocks[i].instructions[-1], 'isConditional'):
+			if re.search("^if-", blocks[i].instructions[-1]) != None:
 				ncolor = "cornflowerblue"
-			elif Block.isTag(blocks[i].instructions[0], 'isLabel'):
+			elif re.search("^\:" , blocks[i].instructions[0]) != None:
 				ncolor = "tan"
-			elif Block.isTag(blocks[i].instructions[-1], 'isJump'):
+			elif re.search("^goto", blocks[i].instructions[-1]) != None:
 				ncolor = "darkgreen"
-			elif Block.isTag(blocks[i].instructions[-1],'isCall'):
+			elif re.search("^invoke-", blocks[i].instructions[-1]) != None:
 				ncolor = "lightyellow4"
 			else:
 				ncolor = "mediumaquamarine"
-			nodes[i] = pydot.Node(b1.label, color=ncolor, style="filled", shape="box", fontname="Courier", fontsize="8")
-			bis="%s\l%s\l" % (blocks[i].label, "\l".join(blocks[i].instructions))
-			nodes[i].set_name(bis)
+
+			insTxt = "%s\r" % blocks[i].label
+			lidx = int(blocks[i].label.split(' ')[-1])
+			for ii in blocks[i].instructions:
+				insTxt += str(lidx) +  ": " + ii
+				lidx+=1
+
+			nodes[i] = pydot.Node(b1.label, color=ncolor, style="filled", shape="box", fontname="Courier", fontsize=8)
+			nodes[i].set_name(insTxt)
 			self.graph.add_node(nodes[i])
 
-		ed = pydot.Edge(nodes[0], nodes[1], color=ecolor, label=label, fontname="Courier", fontsize="8", arrowhead="open")
+		ed = pydot.Edge(nodes[0], nodes[1], color=ecolor, label=label, fontname="Courier", fontsize=8, arrowhead="open")
 		self.graph.add_edge(ed)
 
-	def draw(self, name):
-		self.graph.write_png(name)
+	def draw(self, name, png_mode):
+		#self.graph.write(name)
+		if png_mode:
+			self.graph.write_png(name + ".png")
+		else:
+			self.graph.write(name + ".dot")
 
 class Block(object):
 	""" Sequential group of instructions """
@@ -63,47 +78,14 @@ class Block(object):
 		self.instructions = instructions
 		self.targets = []
 
-	@staticmethod
-	def isTag(inst, ttype):
-		""" Indicates whether the specified code line contains an known instruction. """
-		if ttype == 'isMethodBegin':
-			match = re.search("^.method ", inst)
-		elif ttype == 'isMethodEnd':
-			match = re.search("^.end method", inst)
-		elif ttype == 'isJump':
-			match = re.search("^goto", inst)
-		elif ttype == 'isConditional':
-			match = re.search("^if-", inst)
-		elif ttype == 'isCatch':
-			match = re.search("^.catch ", inst)
-		elif ttype == 'isLabel':
-			#match = re.search("^\:" , inst) and ((not re.search("^\:try" , inst)) and (not re.search("^\:catch" , inst)))
-			match = re.search("^\:" , inst)
-		elif ttype == 'isReturn':
-			match = re.search("^return-", inst)
-		elif ttype == 'isCall':
-			match = re.search("^invoke-", inst)			
-		else:
-			match = None
-		if match:
-			return True
-		else:
-			return False
-
 	def add_inst(self, inst):
 		""" Just add one instruction to our set of instructions. """
 		self.instructions.append(inst)
 
-	def isNeighbor(self, block):
-		""" Specified block is at the same class and method ? """
-		return (block.parent_class  == self.parent_class) and (block.parent_method == self.parent_method)
-
-	def getLength(self, inc=0):
-		return len(self.instructions) + inc
-
 class BlockFactory(object):
 	def __init__(self):
 		self.blocks = []
+		self.ApplicationInventory = []
 
 	def add(self, blk):
 		global graph
@@ -124,8 +106,29 @@ class BlockFactory(object):
 		return b
 
 	@staticmethod
-	def xtractBlock(classfile, functionname):
+	def ApplicationInventory(rdir):
+		full_methods = []
+		for root, subFolders, files in os.walk(rdir):
+			for file in files:
+				if file.endswith(".smali"):
+					with open(root+"/"+file, "r") as file_handle:
+						smali_content = file_handle.read()
+						class_search = re.search('\.class\s+(.*?)\n', smali_content)
+						class_definition = class_search.group(0)[:-1].split(' ')[-1][:-1]
+						method_definitions = re.findall('\.method\s+(.*?)\n', smali_content)
+						for method2 in method_definitions:
+							full_methods.append("%s->%s" % (class_definition, method2.split(' ')[-1][:-1]) )
+		BlockFactory.ApplicationInventory = full_methods
+		return full_methods
+
+	@staticmethod
+	def xtractBlock(rdir, mdef):
 		""" split smali class file into method/s code lines. """
+		ndir = "/".join(mdef.split('->')[0][1:-1].split('/')[:-1])
+		nfil = mdef.split('->')[0][1:-1].split('/')[-1]
+		ndef = mdef.split('->')[1]
+		classfile = "%s%s/%s.smali" % (rdir, ndir, nfil)
+		functionname = ndef
 		# read class file contents
 		fh = open(classfile, "r")
 		fc = fh.read()
@@ -134,7 +137,7 @@ class BlockFactory(object):
 		methods = []
 		for m in re.findall("\.method\s(.*?)\n(.*?)\.end\smethod", fc, re.DOTALL):
 			if functionname is not None:
-				if m[0].split(' ')[-1].split('(')[0] == functionname:
+				if m[0].split(' ')[-1][:-1] == functionname:
 					methods.append(m)
 					break
 			else:
@@ -147,8 +150,8 @@ class BlockFactory(object):
 			for m in methods:
 				instructions = []
 				for inst in m[1].split("\n"):
-					if len(inst)>0:
-						instructions.append( inst.lstrip() )
+					if len(inst.lstrip())>0:
+						instructions.append( inst.lstrip()[:-1] + "\l")
 				mname = m[0].split(' ')[-1].split('(')[0]
 				ret.append((mname,instructions))
 			# All done!
@@ -158,45 +161,77 @@ class BlockFactory(object):
 	def splitBlock(blk, classn, methodn, pos, lenInc, iset, i):
 		blockLen = len(blk.instructions) + lenInc
 		incrementalLabel = "%s %s %d" % (classn, methodn, pos+blockLen)
-		if not Block.isTag(i, 'isCall'):
+		outCall = False
+		if re.search("^invoke-", i) == None:
 			positionalLabel  = "%s %s %d" % (classn, methodn, iset.index(i.split(' ')[-1])+1 )
 		else:
 			lindex = int(blk.label.split(' ')[-1]) + len(blk.instructions)
 			positionalLabel = " ".join(blk.label.split(' ')[:-1]) + " " + str(lindex+1)
-		return (incrementalLabel, positionalLabel)
+			if i.split(' ')[-1][:-2] in BlockFactory.ApplicationInventory:
+				outCall = True
+				nfil = i.split('->')[0][1:-1].split('/')[-1]
+				ndef = i.split('->')[1].split('(')[0]
+				positionalLabel = "%s %s %d"  % (nfil, ndef, 1)
+		return (outCall, incrementalLabel, positionalLabel)
 
-### main block xtraction from smali class file.
-# 1.- block for specified method.
-# 2.- blocks for all methods inside class file.
-def smaliFileReader(file, method):
-	methodInstructions = BlockFactory.xtractBlock(filename, methodname)
-	if methodInstructions is not None:
-		methods.append((classname, methodInstructions[0], methodInstructions[1]))
 
+def MyOptParser():
+	usage = "usage: %prog [options]"
+	parser = OptionParser(usage=usage, epilog="""[-d ./smali -m "Lcom/foo/client;->onSuccess(Ljava/lang/String;I)V"]""")
+	parser.add_option("-d", action="store", type="string", dest="rootdir",
+                  help="Smali code directory entry point.")
+	parser.add_option("-m",  action="store", type="string", dest="method",
+                  help="Full class method definition string to look for.")
+
+	(options, args) = parser.parse_args()
+	if (not options.rootdir):
+		parser.error("option -d is mandatory.")
+	if (not options.method):
+		parser.error("Option -m is mandatory.")
+	return options
+
+def findCrossReferences(rdir, mdef):
+	definitions = [mdef]
+	idx = 0
+	while True:
+		ndef = definitions[idx]
+		ndir = "/".join(ndef.split('->')[0][1:-1].split('/')[:-1])
+		nfil = ndef.split('->')[0][1:-1].split('/')[-1]
+		ndef = ndef.split('->')[1]
+		with open( "%s%s/%s.smali" % (rdir, ndir, nfil) , "r") as fh:
+			content = fh.read()
+			pdef =  content.find(ndef)
+			pdef2 = content.find(".end method", pdef + len(ndef))
+			content = content[pdef:pdef2].replace('\r\n\r\n', '\r\n')
+			calls = re.findall('invoke-(.*?)\n', content)
+			for call in calls:
+				if call.split(' ')[-1][:-1] in full_methods:
+					if call.split(' ')[-1][:-1] not in definitions:
+						definitions.append( call.split(' ')[-1][:-1] )
+		idx += 1
+		if idx >= len(definitions): break
+	return definitions
 
 if __name__ == '__main__':
 
-	## Parsing command line options
-	modes = ['XRefTo','XRefFrom','XRefBoth']
-	usage = "usage: %prog [options]"
-	parser = OptionParser(usage=usage)
-	parser.add_option("-c", "--class", action="store", type="string", dest="filename",
-                  help="Smali class file.")
-	parser.add_option("-m", "--method", action="store", type="string", dest="methodname",
-                  help="Class method to analyze).")
-	
-	## Handling command line options errors.
-	(options, args) = parser.parse_args()
-	if (not options.filename):
-		parser.error("option -c is mandatory.")
+	options = MyOptParser()
+	full_methods = BlockFactory.ApplicationInventory(options.rootdir)
+
+	mdefinition = options.method
+	if mdefinition not in full_methods:
+		sys.exit("Error: Incorrect parameters.")
 
 	graph_mgr = GraphManager()
+	definitions = findCrossReferences(options.rootdir, mdefinition)
 	methods = []
-	classname = "/".join(options.filename.split('.')[0].split('/')[1:])
-	methodInstructions = BlockFactory.xtractBlock(options.filename, options.methodname)
-	if methodInstructions is not None:
-		for mnam, minst in methodInstructions:
-			methods.append((classname, mnam, minst))
+
+	for method in definitions:
+		nfil = method.split('->')[0][1:-1].split('/')[-1]
+		methodInstructions = BlockFactory.xtractBlock(options.rootdir, method)
+		if methodInstructions is not None:
+			for mnam, minst in methodInstructions:
+				methods.append((nfil, mnam, minst))
+
 
 	## linear pass 1
 	# split method code block into smaller blocks (calls/jumps/conditionals/labeled/catch)
@@ -207,26 +242,30 @@ if __name__ == '__main__':
 		b = Block(label=cname + " " + mname + " 1", instructions=[], parent_class = cname, parent_method = mname)
 		for i2 in minsts:
 			instrPos = minsts.index(i2) + 1
-			blockPos = int(b.label.split(' ')[-1])
-			if Block.isTag(i2, 'isJump'):
-				(incLabel, posLabel) = BlockFactory.splitBlock(b, cname, mname, blockPos, 1, minsts, i2)
+			blockPos = int(b.label.split(' ')[-1] )
+			if re.search("^goto", i2) != None:
+				(outCall, incLabel, posLabel) = BlockFactory.splitBlock(b, cname, mname, blockPos, 1, minsts, i2)
 				b.targets = [('jump',posLabel)]
 				b = factory.add_before(label=incLabel, inst=i2, block=b, pclass=cname, pmethod=mname)
-			elif Block.isTag(i2, 'isConditional'):
-				(incLabel, posLabel) = BlockFactory.splitBlock(b, cname, mname, blockPos, 1, minsts, i2)
+			elif re.search("^if-", i2) != None:
+				(outCall, incLabel, posLabel) = BlockFactory.splitBlock(b, cname, mname, blockPos, 1, minsts, i2)
 				b.targets = [('true',posLabel), ('false',incLabel)]
 				b = factory.add_before(label=incLabel, inst=i2, block=b, pclass=cname, pmethod=mname)
-			elif Block.isTag(i2, 'isLabel'):
-				(incLabel, posLabel) = BlockFactory.splitBlock(b, cname, mname, blockPos, 0, minsts, i2)
+			elif re.search("^\:" , i2) != None:
+				(outCall, incLabel, posLabel) = BlockFactory.splitBlock(b, cname, mname, blockPos, 0, minsts, i2)
 				b.targets = [('cont',incLabel)]
 				b = factory.add_after(label=incLabel, inst=i2, block=b, pclass=cname, pmethod=mname)
-			elif Block.isTag(i2, 'isCatch'):
-				(incLabel, posLabel) = BlockFactory.splitBlock(b, cname, mname, blockPos, 1, minsts, i2)
+			elif re.search("^.catch ", i2) != None:
+				(outCall, incLabel, posLabel) = BlockFactory.splitBlock(b, cname, mname, blockPos, 1, minsts, i2)
 				b.targets = [('exception',posLabel), ('try',incLabel)]
-				b = factory.add_before(label=incLabel, inst=i2, block=b, pclass=cname, pmethod=mname)			
-			elif Block.isTag(i2, 'isCall'):
-				(incLabel, posLabel) = BlockFactory.splitBlock(b, cname, mname, blockPos, 1, minsts, i2)
-				b.targets = [('on return',posLabel)]
+				b = factory.add_before(label=incLabel, inst=i2, block=b, pclass=cname, pmethod=mname)
+			elif re.search("^invoke-", i2) != None:
+				(outCall, incLabel, posLabel) = BlockFactory.splitBlock(b, cname, mname, blockPos, 1, minsts, i2)
+				if outCall:
+					b.targets = [('call',posLabel), ('on return', incLabel)]
+				else:
+					b.targets = [('on return',posLabel)]
+
 				b = factory.add_before(label=incLabel, inst=i2, block=b, pclass=cname, pmethod=mname)
 			else:
 				b.add_inst(i2)
@@ -240,5 +279,5 @@ if __name__ == '__main__':
 				if b2.label == target:
 					graph_mgr.add_edge(b1,b2, lbl)
 					break
-
-	graph_mgr.draw('_flow.png')
+	fname = options.method.split('->')[1].split('(')[0]
+	graph_mgr.draw(fname, True)
